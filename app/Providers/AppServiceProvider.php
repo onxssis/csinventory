3f6baa59b7f95960aa2d288ced5a2a183a1fc627
Ona <onxssis@gmail.com>
2018-03-01 16:50:53 +0100

    redirect to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
         if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
-        
+
         Blade::if('admin', function () {
             return auth()->check() && auth()->user()->hasRole('Admin');
         });
