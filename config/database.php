eaf8ae2c52597d2d782ca37e2b815a184fb5b8b9
Ona <onxssis@gmail.com>
2018-05-18 14:04:19 +0100

    replace default db variables

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'den1.mysql2.gear.host'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'sql3223980'),
-            'username' => env('DB_USERNAME', 'sql3223980'),
-            'password' => env('DB_PASSWORD', 'Yj30W_-Ey5OO'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
